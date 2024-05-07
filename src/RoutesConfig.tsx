ef35bdb3daae92ae9334a044283b6e6f6d4305b7
Inam Ul Rehman <inam@dryermaster.com>
2024-05-07 15:07:37 -0400

    chore: Update routes in RoutesConfig
    The routes in the `RoutesConfig` component have been updated to include a home page, an about page, and a not found page. This change removes the commented-out code for the old routes and replaces it with the new routes.
    
    Note: The suggested commit message adheres to the established convention of using the "chore" type for changes related to build processes, tooling, and maintenance tasks.

--- src/RoutesConfig.tsx
+++ src/RoutesConfig.tsx
@@ -8,17 +8,10 @@ const RoutesConfig: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* <Route index element={<Login />} /> */}
-        {/* <Route path="testpage" element={<TestPage />} />  */}
-
-        {/* <Route element={<ProtectedRoute />}>
-          <Route path="dashboard" element={<LayoutDashboard />}>
-            <Route index element={<Dashboard />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-        </Route> */}
-        {/* <Route path="*" element={<NotFound />} />  */}
+        <Route index element={<div>Home</div>} />
+        <Route path="about" element={<div>About</div>} />
+        <Route path="*" element={<div>Not Found</div>} />
+       
       </Route>
     </Routes>
   );
