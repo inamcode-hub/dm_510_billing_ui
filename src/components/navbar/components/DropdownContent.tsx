132b64ee93fe591c9fc168fb9c973ba5738d5ae3
Inam Ul Rehman <inam@dryermaster.com>
2024-05-08 10:38:35 -0400

    refactor: Improve mobile navigation bar styling

---
+++ src/components/navbar/components/DropdownContent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DropdownContent: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>DropdownContent</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default DropdownContent;
--- src/components/navbar/components/Header.tsx
+++ src/components/navbar/components/Header.tsx
@@ -2,15 +2,31 @@ import styled from "@emotion/styled";
 import React from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
+import DropdownContent from "./DropdownContent";
 const Header: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleAction = () => {
+    setOpen(!open);
+  };
+
   return (
     <React.Fragment>
       <Wrapper>
-        <div className="logo">
-          <Link to="/">DM Mobile</Link>
+        <div className="header-content">
+          <div className="logo">
+            <Link to="/">DM Mobile</Link>
+          </div>
+          <button className="icon" onClick={handleAction}>
+            <MenuIcon style={{ color: "white", fontSize: 28 }} />
+          </button>
         </div>
-        <div className="icon">
-          <MenuIcon style={{ color: "white", fontSize: 28 }} />
+        <div
+          className={
+            open ? "drop-down show-drop-down" : "drop-down hide-drop-down"
+          }
+        >
+          <DropdownContent />
         </div>
       </Wrapper>
     </React.Fragment>
@@ -18,38 +34,59 @@ const Header: React.FC = () => {
 };
 
 const Wrapper = styled.div`
-  background-color: var(--menu-color);
-  display: flex;
-  justify-content: space-between;
-  min-height: 50px;
-  .logo {
-    a {
-      float: left;
-      height: 50px;
-      padding: 15px 15px;
-      font-size: 18px;
-      line-height: 20px;
-      color: var(--menu-text-color);
-      // hover
+  position: relative;
+  .header-content {
+    background-color: var(--menu-color);
+    display: flex;
+    justify-content: space-between;
+    min-height: 50px;
+    .logo {
+      a {
+        float: left;
+        height: 50px;
+        padding: 15px 15px;
+        font-size: 18px;
+        line-height: 20px;
+        color: var(--menu-text-color);
+        // hover
+        :hover {
+          color: #fff;
+        }
+      }
+    }
+    .icon {
+      border: 1px solid #333;
+      border-radius: 2px;
+      margin: 10px;
+      padding: 5px;
+      margin-right: 15px;
+      margin-top: 8px;
+      background-color: transparent;
       :hover {
-        color: #fff;
+        background-color: #333;
+      }
+
+      svg {
+        margin: -3px 2px -7px 3px;
       }
     }
   }
-  .icon {
-    border: 1px solid #333;
-    border-radius: 2px;
-    margin: 10px;
-    padding: 5px;
-    margin-right: 15px;
-    margin-top: 8px;
-    :hover {
-      background-color: #333;
-    }
-
-    svg {
-      margin: -3px 2px -7px 3px;
-    }
+  .drop-down {
+    background-color: var(--menu-color);
+    position: absolute;
+    width: 100%;
+    overflow-y: hidden;
+    border-top: 1px solid #333;
+    // box shadow top
+    padding: 0 15px;
+  }
+  .show-drop-down {
+    transition: height 0.3s;
+    height: 150px;
+  }
+  .hide-drop-down {
+    transition: height 0.3s;
+    height: 0;
   }
 `;
 export default Header;
