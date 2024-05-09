f83fedbb34b09926d74d10202d65f61d88f77011
Inam Ul Rehman <inam@dryermaster.com>
2024-05-09 13:30:41 -0400

    refactor: Update DropdownContent component structure and styling
    This commit refactors the DropdownContent component in the navbar_menu directory. It updates the component structure to use a dynamic list of navigation items instead of hardcoding them. It also improves the styling by adjusting the margin and spacing of the icon. These changes enhance the flexibility and maintainability of the component.
    
    Note: The suggested commit message follows the established convention of using the "refactor" type for changes that improve the code structure, organization, or design without changing its functionality.

--- src/components/navbar_menu/components/DropdownContent.tsx
+++ src/components/navbar_menu/components/DropdownContent.tsx
@@ -1,35 +1,47 @@
 import styled from "@emotion/styled";
-import { FaChartBar, FaCog, FaHistory, FaPowerOff } from "react-icons/fa"; // Import the required icons
+import { FaChartBar, FaCog, FaHistory, FaPowerOff } from "react-icons/fa";
 import { AiOutlineDashboard } from "react-icons/ai";
+
+const navItems = [
+  {
+    href: "https://my.dryermaster.com/users/dashboard",
+    icon: <AiOutlineDashboard />,
+    label: "Dashboard",
+  },
+  {
+    href: "https://my.dryermaster.com/users/charts",
+    icon: <FaChartBar />,
+    label: "Charts",
+    active: true,
+  },
+  {
+    href: "https://my.dryermaster.com/users/history",
+    icon: <FaHistory />,
+    label: "History",
+  },
+  {
+    href: "https://my.dryermaster.com/users/preferences",
+    icon: <FaCog />,
+    label: "Preferences",
+  },
+  {
+    href: "https://my.dryermaster.com/users/dashboard",
+    icon: <FaPowerOff />,
+    label: "Log Out",
+  },
+];
+
 const DropdownContent = () => {
   return (
     <Wrapper>
-      <ul className="nav navbar-nav side-nav">
-        <li>
-          <a href="https://my.dryermaster.com/users/dashboard">
-            <AiOutlineDashboard /> Dashboard
-          </a>
-        </li>
-        <li className="active">
-          <a href="https://my.dryermaster.com/users/charts">
-            <FaChartBar /> Charts
-          </a>
-        </li>
-        <li>
-          <a href="https://my.dryermaster.com/users/history">
-            <FaHistory /> History
-          </a>
-        </li>
-        <li>
-          <a href="https://my.dryermaster.com/users/preferences">
-            <FaCog /> Preferences
-          </a>
-        </li>
-        <li>
-          <a href="https://my.dryermaster.com/users/dashboard">
-            <FaPowerOff /> Log Out
-          </a>
-        </li>
+      <ul>
+        {navItems.map((item, index) => (
+          <li key={index} className={item.active ? "active" : ""}>
+            <a href={item.href}>
+              {item.icon} {item.label}
+            </a>
+          </li>
+        ))}
       </ul>
     </Wrapper>
   );
@@ -49,6 +61,7 @@ const Wrapper = styled.div`
       line-height: 20px;
       display: flex;
       align-items: center;
+
       :hover {
         cursor: pointer;
       }
@@ -56,23 +69,26 @@ const Wrapper = styled.div`
       a {
         color: var(--menu-text-color);
         padding-left: 2px;
-        /* background-color: pink; */
         width: 100%;
         padding: 10px 15px;
         margin-left: 3px;
+
         @media (min-width: 768px) {
           padding: 15px 15px;
+
           :hover {
             background-color: #080808;
+
             a {
               color: #fff;
             }
           }
         }
+
         :hover {
           color: #fff;
         }
-        // icon
+
         svg {
           margin-right: 3px;
           margin-left: -2px;
@@ -81,11 +97,14 @@ const Wrapper = styled.div`
       }
     }
   }
+
   .active {
     background-color: #080808;
+
     a {
       color: #fff;
     }
   }
 `;
+
 export default DropdownContent;
