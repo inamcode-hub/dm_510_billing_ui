1425a6dbd63b90465ac9a19d35fd794e4cb7b2fa
Inam Ul Rehman <inam@dryermaster.com>
2024-05-22 14:01:28 -0400

    feat: Add form for user name input in Payment page

--- src/pages/payment/payment/Payment.tsx
+++ src/pages/payment/payment/Payment.tsx
@@ -1,11 +1,48 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import NavigatePages from '../components/NavigatePages';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { setShowPackage } from '../../../lib/redux/features/payment/paymentSlice';
+
+const initialState = {
+  input: '',
+};
 
 const Payment: React.FC = () => {
-  const { showPayment } = useSelector((state: any) => state.payment);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { showPayment } = useSelector((state: any) => state.payment);
+
+  const [state, setState] = useState(initialState);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (state.input.trim() === '') {
+      alert('Please enter your name');
+    } else {
+      // console.log('Form submitted with name:', state.input);
+      alert('Payment successful');
+    }
+  };
+  const handleBack = () => {
+    dispatch(setShowPackage());
+  };
 
   useEffect(() => {
     // if showPayment page is true show the payment otherwise redirect to the package
@@ -17,7 +54,54 @@ const Payment: React.FC = () => {
     <React.Fragment>
       <div>
         <NavigatePages />
-        <h1>Payment page</h1>
+        <Card
+          variant="outlined"
+          sx={{
+            width: '90%',
+            margin: 'auto',
+            marginTop: '50px',
+            padding: '20px',
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Payment page
+              </Typography>
+              <TextField
+                id="input"
+                name="input"
+                label="Name"
+                placeholder="Enter your name"
+                fullWidth
+                margin="normal"
+                value={state.input}
+                onChange={onChange}
+                variant="outlined"
+                required
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                type="button"
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: 'auto' }}
+                onClick={handleBack}
+              >
+                Previous
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: 'auto' }}
+              >
+                Next
+              </Button>
+            </CardActions>
+          </form>
+        </Card>
       </div>
     </React.Fragment>
   );
