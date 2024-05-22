1764629dc60f12dc2a0818cb1813d40754f5b952
Inam Ul Rehman <inam@dryermaster.com>
2024-05-22 13:52:31 -0400

    feat: Add form for user name input in Package page
    This commit adds a form to the Package page in the src/pages/payment/package/Package.tsx file. The form includes an input field for users to enter their name. When the form is submitted, the setShowPayment action from paymentSlice is dispatched. This enhancement improves the user experience by allowing users to provide their name for further processing.
    
    Note: The suggested commit message follows the established convention of using the "feat" type for new features or enhancements.

--- src/pages/payment/package/Package.tsx
+++ src/pages/payment/package/Package.tsx
@@ -1,11 +1,51 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import NavigatePages from '../components/NavigatePages';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Typography,
+} from '@mui/material';
+import {
+  setShowPayment,
+  setShowProfile,
+} from '../../../lib/redux/features/payment/paymentSlice';
+
+const initialState = {
+  input: '',
+};
 
 const Package: React.FC = () => {
-  const { showPackage } = useSelector((state: any) => state.payment);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { showPackage } = useSelector((state: any) => state.payment);
+
+  const [state, setState] = useState(initialState);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (state.input.trim() === '') {
+      alert('Please enter your name');
+    } else {
+      // console.log('Form submitted with name:', state.input);
+      dispatch(setShowPayment());
+    }
+  };
+  const handleBack = () => {
+    dispatch(setShowProfile());
+  };
   useEffect(() => {
     // if showPackage page is true show the package otherwise redirect to the payment
     if (!showPackage) {
@@ -16,7 +56,54 @@ const Package: React.FC = () => {
     <React.Fragment>
       <div>
         <NavigatePages />
-        <h1>Package page</h1>
+        <Card
+          variant="outlined"
+          sx={{
+            width: '90%',
+            margin: 'auto',
+            marginTop: '50px',
+            padding: '20px',
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Package
+              </Typography>
+              <TextField
+                id="input"
+                name="input"
+                label="Name"
+                placeholder="Enter your name"
+                fullWidth
+                margin="normal"
+                value={state.input}
+                onChange={onChange}
+                variant="outlined"
+                required
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                type="button"
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: 'auto' }}
+                onClick={handleBack}
+              >
+                Previous
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: 'auto' }}
+              >
+                Next
+              </Button>
+            </CardActions>
+          </form>
+        </Card>
       </div>
     </React.Fragment>
   );
