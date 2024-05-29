145cab5f4aca7230cdce4b0bf919f311489ef22a
Inam Ul Rehman <inam@dryermaster.com>
2024-05-29 10:12:53 -0400

    refactor: Update Package component to include rollup-plugin-visualizer
    This commit adds the rollup-plugin-visualizer package to the project dependencies in the package.json file. It also updates the vite.config.ts file to include the visualizer plugin in the Vite configuration. This enhancement improves the build process by providing a visual representation of the bundle size and dependencies, helping developers optimize the application's performance and reduce the bundle size.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ open: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const modules = id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+            return `vendor/${modules}`;
+          }
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
+  },
 });
