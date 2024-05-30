ccf7e02fe7eec0656f56f094bd44da475198fe1f
Inam Ul Rehman <inam@dryermaster.com>
2024-05-30 08:21:02 -0400

    refactor: Update react-toastify to version 10.0.5
    Update the react-toastify package to version 10.0.5 in package.json and package-lock.json. This update includes bug fixes and improvements to the toast notification component, enhancing the user experience and ensuring compatibility with the latest versions of React and React DOM.

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesConfig from './RoutesConfig';
-import Provider from './lib/redux/Provider.tsx'
-import { ThemeProvider } from '@emotion/react'
-import { GlobalStyles } from '@mui/system'
-import theme from './lib/styles/theme.ts'
+import Provider from './lib/redux/Provider.tsx';
+import { ThemeProvider } from '@emotion/react';
+import { GlobalStyles } from '@mui/system';
+import theme from './lib/styles/theme.ts';
 import { globalStyles } from './lib/styles/GlobalStyles.js';
-import './lib/styles/index.css'
-
+import './lib/styles/index.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider>
       <ThemeProvider theme={theme}>
-      <GlobalStyles styles={globalStyles} />  {/* Use global styles */}
-      <Router>
+        <GlobalStyles styles={globalStyles} /> {/* Use global styles */}
+        <Router>
           <RoutesConfig />
         </Router>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>,
-)
+    <ToastContainer />
+  </React.StrictMode>
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import NavigatePages from './payment/components/NavigatePages';
 
 const Home: React.FC = () => {
+  useEffect(() => {}, []);
   return (
     <React.Fragment>
       <Wrapper>
